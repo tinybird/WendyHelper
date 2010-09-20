d26d98c00ecfc7e16fffde2e4a0a344cb676b55f
Richard Hult <richard@tinybird.com>
2010-09-20 10:45:21 +0200

    Initial commit

---
+++ WendyHelper.m
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+#import "ReportManager.h"
+
+
+int main (int argc, const char * argv[])
+{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+
+    
+    
+    [pool drain];
+
+    return 0;
+}
